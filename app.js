3fef70144950a892b739f299a502159359e68c21
Courtney Fay <fay.courtney@gmail.com>
2017-09-21 17:46:55 -0600

    adds new html css and js files, and comments in all the requirements for the assignment

---
+++ app.js
@@ -0,0 +1,19 @@
+/* JS Requirements
+	
+-Your game page must have at least two player objects											**NOT YET**
+-Your game must have a clear win condition														**NOT YET**
+-Add a keydown event listener that console logs "blue move" or "red move"						**NOT YET**
+-Replace that console log with JavaScript that moves the red or blue div a little to the right 	**NOT YET** 
+-GRADED: Adhere to the AirBnB style guide for writing your JavaScript							**NOT YET**
+-GRADED: Comment your code appropriately														**NOT YET**
+
+Bonus:
+-Have players do something other than simply pressing a key to move their player, 
+	for example, make them type words, or solve simple math problems
+-Score each game based on the time it took to complete and by how much time 
+	they beat the opposing player
+-Display the time it took for the player to complete the race
+-Make a 'Best of 3' tournament and display the winner of the tournament
+-Keep track of best times/ high scores across page refreshes using localStorage
+
+*/
---
+++ main.cs
@@ -0,0 +1,16 @@
+/* CSS Requirements
+
+-Create HTML and CSS files that create one red square div and one blue, **NOT YET**
+	lined up on the left side of the screen
+-Style your landing page and game page with class-based CSS 			**NOT YET**
+-Include a media query for a tablet to make your pages responsive 		**NOT YET**
+	(use % instead of px to determine winnah) 							**NOT YET**
+-GRADED: Adhere to the Google style guide for writing your CSS  		**NOT YET**
+-GRADED: Comment your code appropriately								**NOT YET**
+
+Bonus:
+-Use images to create a background and/or players						
+-Add animations!!!!
+-snowboarding theme
+
+*/
