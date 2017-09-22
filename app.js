3e45c61e7137d64b8a612a2285193e81a588f167
Courtney Fay <fay.courtney@gmail.com>
2017-09-21 23:27:48 -0600

    adds JS that creates player objects, a prototype function for movement, and an event listener function for keystroke

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 /* JS Requirements
 	
--Your game page must have at least two player objects											**NOT YET**
+-Your game page must have at least two player objects											DONE
+-Add a keydown event listener that console logs "blue move" or "red move"						DONE
+-Replace that console log with JavaScript that moves the red or blue div a little to the right 	DONE 
 -Your game must have a clear win condition														**NOT YET**
--Add a keydown event listener that console logs "blue move" or "red move"						**NOT YET**
--Replace that console log with JavaScript that moves the red or blue div a little to the right 	**NOT YET** 
 -GRADED: Adhere to the AirBnB style guide for writing your JavaScript							**NOT YET**
 -GRADED: Comment your code appropriately														**NOT YET**
 
@@ -17,5 +17,43 @@ Bonus:
 -Keep track of best times/ high scores across page refreshes using localStorage
 
 */
+document.addEventListener('DOMContentLoaded', function() {
+  console.log("js is loaded!");
 
-console.log("js is loaded!");
+  // Constructor function that creates 2 new player objects
+  function Player(name="player1", position=0) {
+  		this.name = name;
+  		this.position = position;
+  }
+
+  //function that moves the player 10px right when the appropriate key is struck (k/K for player1, s/S for player2)
+  Player.prototype = {
+  	keystrokeMove: function() {
+  		this.position = this.position + 10;
+  		let player = document.getElementById(this.name);
+  		let newPixelPosition = this.position + "px";
+  		player.style.left = newPixelPosition;
+  	}
+  }; 
+
+  //setting some global objects and variables
+  let player1 = new Player("player1", 0);
+  let player2 = new Player("player2", 0);
+
+  //function that listens for an event of keypress on the document and then decides which player (k/K is player1, s/S is player2)
+  document.addEventListener("keypress",keypressListener);
+
+  function keypressListener() {
+  	if (event.key == "k" || event.key == "K") {	
+	  player1.keystrokeMove();
+	} else if (event.key == "s" || event.key == "S") {
+		 player2.keystrokeMove();
+	} else {
+		console.log("you pressed the wrong key");
+		console.log(event);
+		console.log(event.key);
+	};
+  };
+});
+
+
