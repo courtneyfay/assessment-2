4dd5c4f252925edc658b40a0f00af4bbfd4c0e5f
Courtney Fay <fay.courtney@gmail.com>
2017-09-22 23:24:21 -0600

    styles the HTML and CSS according to Google styleguides

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-/* JS Requirements
+/* TODO: JS Requirements
 	
--Your game page must have at least two player objects											DONE
--Add a keydown event listener that console logs "blue move" or "red move"						DONE
+-Your game page must have at least two player objects																						DONE
+-Add a keydown event listener that console logs "blue move" or "red move"												DONE
 -Replace that console log with JavaScript that moves the red or blue div a little to the right 	DONE 
--Your game must have a clear win condition														**NOT YET**
--GRADED: Adhere to the AirBnB style guide for writing your JavaScript							**NOT YET**
--GRADED: Comment your code appropriately														**NOT YET**
+-Your game must have a clear win condition																											DONE
+-GRADED: Adhere to the AirBnB style guide for writing your JavaScript														**NOT YET**
+-GRADED: Comment your code appropriately																												**NOT YET**
+	-Use comments to explain code: What does it cover, what purpose does it serve, why is respective solution used or preferred?
 
 Bonus:
 -Have players do something other than simply pressing a key to move their player, 
@@ -26,36 +27,46 @@ document.addEventListener('DOMContentLoaded', function() {
   		this.xPosition = xPosition;
   		this.yPosition = yPosition;
   		this.width = width;
+  		this.element = document.getElementById(name);
   }
 
-  //function that moves the player 1% to the right when the appropriate key is struck (k/K for player1, s/S for player2)
+  // function that makes the player ride when the appropriate key is struck (k/K for player1, s/S for player2)
   Player.prototype = {
   	keystrokeMove: function() {
-  		let player = document.getElementById(this.name);
 
   		//move the player to the right
   		this.xPosition = this.xPosition + 9;
-  		player.style.left = (this.xPosition + "px");
+  		this.element.style.left = (this.xPosition + "px");
 
   		//moves the player down the mountain
   		this.yPosition = this.yPosition + 4;
-  		player.style.top = (this.yPosition + "px"); 
+  		this.element.style.top = (this.yPosition + "px");
+
+  		this.checkForWinner(); 
   	}, 
   	
+  	//after every key press, check for a winner
   	checkForWinner: function() {
   		let snowboarderPosition = this.xPosition + this.width;	
 
   		if (snowboarderPosition < racetrack.offsetWidth) {
   			return;
   		} else {
-  			winAnimation(this.name);
+  			this.winAnimation();
   		}
+  	}, 
+
+  	// when a player wins, they do a flip
+  	winAnimation: function() {
+  		this.element.classList.add("animated");
+  		document.removeEventListener("keypress",keypressListener);
+  		//how to let end users know who won WITHOUT using an alert?
   	}
   }; 
 
   //setting some global objects and variables
-  let player1 = new Player("player1", 0, 0);
-  let player2 = new Player("player2", 0, 0);
+  let player1 = new Player("player1", 0, 0, 100);
+  let player2 = new Player("player2", 0, 0, 100);
   let racetrack = document.getElementById('container');
 
   //function that listens for an event of keypress on the document and then decides which player to move (k/K is player1, s/S is player2)
@@ -72,13 +83,6 @@ document.addEventListener('DOMContentLoaded', function() {
 	  console.log(event.key);
 	}
 	
-	//after every key press, check for a winner
-	player1.checkForWinner();
-	player2.checkForWinner();
-  };
-
-  function winAnimation(name) {
-  	console.log(name + " won!");
   };
 
 });
