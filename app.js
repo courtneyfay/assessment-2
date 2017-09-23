3dad02798256c8d63e6cc7f0d01d9f88744e3f18
Courtney Fay <fay.courtney@gmail.com>
2017-09-22 21:36:29 -0600

    styles the snowboarders and adds some checking for win status

--- app.js
+++ app.js
@@ -21,10 +21,11 @@ document.addEventListener('DOMContentLoaded', function() {
   console.log("js is loaded!");
 
   // Constructor function that creates 2 new player objects
-  function Player(name="player1", xPosition=0, yPosition=0) {
+  function Player(name="player1", xPosition=0, yPosition=0, width=100) {
   		this.name = name;
   		this.xPosition = xPosition;
   		this.yPosition = yPosition;
+  		this.width = width;
   }
 
   //function that moves the player 1% to the right when the appropriate key is struck (k/K for player1, s/S for player2)
@@ -33,41 +34,51 @@ document.addEventListener('DOMContentLoaded', function() {
   		let player = document.getElementById(this.name);
 
   		//move the player to the right
-  		this.xPosition = this.xPosition + 1;
-  		let newXPosition = this.xPosition + "%";
-  		player.style.left = newXPosition;
+  		this.xPosition = this.xPosition + 9;
+  		player.style.left = (this.xPosition + "px");
 
   		//moves the player down the mountain
-  		this.yPosition = this.yPosition + 5;
-  		let newYPosition = this.yPosition + "px";
-  		player.style.top = newYPosition;
-  		console.log(newYPosition);
+  		this.yPosition = this.yPosition + 4;
+  		player.style.top = (this.yPosition + "px"); 
+  	}, 
+  	
+  	checkForWinner: function() {
+  		let snowboarderPosition = this.xPosition + this.width;	
+
+  		if (snowboarderPosition < racetrack.offsetWidth) {
+  			return;
+  		} else {
+  			winAnimation(this.name);
+  		}
   	}
   }; 
 
   //setting some global objects and variables
   let player1 = new Player("player1", 0, 0);
   let player2 = new Player("player2", 0, 0);
+  let racetrack = document.getElementById('container');
 
-  //function that listens for an event of keypress on the document and then decides which player (k/K is player1, s/S is player2)
+  //function that listens for an event of keypress on the document and then decides which player to move (k/K is player1, s/S is player2)
   document.addEventListener("keypress",keypressListener);
 
   function keypressListener() {
     if (event.key == "k" || event.key == "K") {	
 	  player1.keystrokeMove();
-	  checkForWinner();
 	} else if (event.key == "s" || event.key == "S") {
 	  player2.keystrokeMove();
-	  checkForWinner();
 	} else {
 	  console.log("you pressed the wrong key");
 	  console.log(event);
 	  console.log(event.key);
-	};
+	}
+	
+	//after every key press, check for a winner
+	player1.checkForWinner();
+	player2.checkForWinner();
   };
 
-  function checkForWinner() {
-  	console.log("I'm checking for a winnah!");
+  function winAnimation(name) {
+  	console.log(name + " won!");
   };
 
 });
